82460a9927b6249735e9c9f9a95c4bd8b00e0d84
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-06-15 18:43:23 +0430

    added logic in aside service

--- src/app/pages/layout/services/aside-service.service.ts
+++ src/app/pages/layout/services/aside-service.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AsideServiceService {
+export class SidenavService {
+  private sidenav: MatSidenav;
 
-  constructor() { }
+  public setSidenav(sidenav: MatSidenav) {
+    this.sidenav = sidenav;
+  }
+
+  public open() {
+    return this.sidenav.open();
+  }
+
+  public close() {
+    return this.sidenav.close();
+  }
+
+  public toggle(): void {
+    this.sidenav.toggle();
+    console.log(this.sidenav);
+  }
 }
