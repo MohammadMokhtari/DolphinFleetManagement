972970f4998525e8bf47c6e4d8bb80c991681678
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-06-15 18:34:41 +0430

    created layout module

---
+++ src/app/pages/layout/layout.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout.component';
+import { HomeComponent } from './components/home/home.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { HeaderComponent } from './components/header/header.component';
+
+
+
+@NgModule({
+  declarations: [
+    LayoutComponent,
+    HomeComponent,
+    AsideComponent,
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LayoutModule { }
