25980858216b64e2ed4b0200cfebb7ed04d7aeb5
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-06-15 18:45:07 +0430

    import requiment material Modules in shard module

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,42 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTabsModule } from '@angular/material/tabs';
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatListModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatTabsModule,
+  ],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatListModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatTabsModule,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
