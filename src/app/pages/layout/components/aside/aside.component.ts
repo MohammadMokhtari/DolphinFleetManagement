7257bbddd651bd495fcc265c2c7e7ae8f690047d
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-06-15 19:04:15 +0430

    inject sidenavservic in aside component

--- src/app/pages/layout/components/aside/aside.component.ts
+++ src/app/pages/layout/components/aside/aside.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+
+import { SidenavService } from '../../services/aside-service.service';
 
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
-  styleUrls: ['./aside.component.scss']
+  styleUrls: ['./aside.component.scss'],
 })
-export class AsideComponent implements OnInit {
+export class AsideComponent implements OnInit, AfterViewInit {
+  constructor(private readonly sidenavService: SidenavService) {}
 
-  constructor() { }
+  @ViewChild('sidenav') public sidenav: MatSidenav;
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.sidenavService.setSidenav(this.sidenav);
   }
-
 }
