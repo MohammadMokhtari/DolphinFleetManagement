7b380eeeda7748c754643b4871b171c736e247ee
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-06-15 19:35:13 +0430

    inject slidenavService in header component

--- src/app/pages/layout/components/header/header.component.ts
+++ src/app/pages/layout/components/header/header.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SidenavService } from '../../services/aside-service.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(private readonly slidenavService: SidenavService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  asideOpend: boolean = false;
+  onToggleAside() {
+    this.slidenavService.toggle();
+    this.asideOpend = !this.asideOpend;
   }
-
 }
