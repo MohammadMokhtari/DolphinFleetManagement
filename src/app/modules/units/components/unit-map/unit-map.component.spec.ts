d7bd5d40b96ac9dfe06cbf85972a825b3bf54952
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-06-16 10:57:39 +0430

    created unitMap component

---
+++ src/app/modules/units/components/unit-map/unit-map.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UnitMapComponent } from './unit-map.component';
+
+describe('UnitMapComponent', () => {
+  let component: UnitMapComponent;
+  let fixture: ComponentFixture<UnitMapComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UnitMapComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UnitMapComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/units/components/unit-map/unit-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-unit-map',
+  templateUrl: './unit-map.component.html',
+  styleUrls: ['./unit-map.component.scss']
+})
+export class UnitMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
