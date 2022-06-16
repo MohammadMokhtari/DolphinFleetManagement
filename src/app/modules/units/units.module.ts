9c7b45661041f9ff42b891f5cea9cf6a8c45385f
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-06-16 10:58:59 +0430

    declear unitList , unitListItem, unitMap component in unitModule

--- src/app/modules/units/units.module.ts
+++ src/app/modules/units/units.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UnitsComponent } from './units.component';
 import { UnitsRoutingModule } from './units-routing.module';
+import { UnitListComponent } from './components/unit-list/unit-list.component';
+import { UnitListItemComponent } from './components/unit-list/unit-list-item/unit-list-item.component';
+import { UnitMapComponent } from './components/unit-map/unit-map.component';
 
 @NgModule({
-  declarations: [UnitsComponent],
+  declarations: [
+    UnitsComponent,
+    UnitListComponent,
+    UnitListItemComponent,
+    UnitMapComponent,
+  ],
   imports: [CommonModule, UnitsRoutingModule],
 })
 export class UnitsModule {}
