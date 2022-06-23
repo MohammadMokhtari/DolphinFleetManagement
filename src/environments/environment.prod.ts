b2a9afaae89ef835eb3c87c582515c7e8205f93c
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-06-23 09:00:38 +0430

    added mapbox accesstoken key in enviroment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox: {
+    accessToken:
+      'pk.eyJ1IjoibW9oYW1tYWQ4IiwiYSI6ImNsNG1reTFjdDExMjkzcWxoNmtudnEydWcifQ.J0psrOjfpyUlTXp1GYr66A',
+  },
 };
