3b18436348547184bb64ea89c873efb05912027b
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-06-23 09:02:43 +0430

    added setRTLTextPlugin for mapbox in appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    mapboxgl.setRTLTextPlugin(
+      'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js',
+      (error: any) => {},
+      true // Lazy load the plugin
+    );
+  }
   title = 'DolphinFleetManagement';
 }
