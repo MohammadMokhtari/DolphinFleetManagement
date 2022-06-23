04e60c618da672b595a6679c04ffae414d1a80ce
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-06-23 09:00:22 +0430

    added mapbox accesstoken key in enviroment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapbox: {
+    accessToken:
+      'pk.eyJ1IjoibW9oYW1tYWQ4IiwiYSI6ImNsNG1reTFjdDExMjkzcWxoNmtudnEydWcifQ.J0psrOjfpyUlTXp1GYr66A',
+  },
 };
 
 /*
