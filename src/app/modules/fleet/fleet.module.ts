8d8e975384b8964fb17f4ba5fd0260731de2df18
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-06-24 09:56:37 +0430

    import fleet routing module in fleet module

--- src/app/modules/fleet/fleet.module.ts
+++ src/app/modules/fleet/fleet.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { FleetComponent } from './fleet.component';
-
-
+import { FleetRoutingModule } from './fleet-routing.module';
 
 @NgModule({
-  declarations: [
-    FleetComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [FleetComponent],
+  imports: [CommonModule, FleetRoutingModule],
 })
-export class FleetModule { }
+export class FleetModule {}
