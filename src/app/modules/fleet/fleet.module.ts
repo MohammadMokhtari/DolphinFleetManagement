ba8b2bacdef4695be9545899cfadf43c5b9a9222
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-06-24 09:57:54 +0430

    import shared module in fleet module

--- src/app/modules/fleet/fleet.module.ts
+++ src/app/modules/fleet/fleet.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { FleetComponent } from './fleet.component';
 import { FleetRoutingModule } from './fleet-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [FleetComponent],
-  imports: [CommonModule, FleetRoutingModule],
+  imports: [CommonModule, FleetRoutingModule, SharedModule],
 })
 export class FleetModule {}
