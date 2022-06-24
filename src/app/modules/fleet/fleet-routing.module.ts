6fb37d27bd6be81c49c21de25936d4f5e741a7ce
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-06-24 16:48:29 +0430

    added child detail route in fleetRoutingModule

--- src/app/modules/fleet/fleet-routing.module.ts
+++ src/app/modules/fleet/fleet-routing.module.ts
@@ -1,11 +1,19 @@
-import { FleetComponent } from './fleet.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { FleetComponent } from './fleet.component';
+import { FleetUnitDetailComponent } from './components/fleet-unit-detail/fleet-unit-detail.component';
 
 const fleetRoutes: Routes = [
   {
     path: '',
     component: FleetComponent,
+    children: [
+      {
+        path: ':id',
+        component: FleetUnitDetailComponent,
+      },
+    ],
   },
 ];
 
