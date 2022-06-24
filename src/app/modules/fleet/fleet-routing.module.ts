14f8688bc58f92804aeb9cb4218f16c48ed05e6b
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-06-24 09:55:00 +0430

    config fleet routes in fleet routing module

--- src/app/modules/fleet/fleet-routing.module.ts
+++ src/app/modules/fleet/fleet-routing.module.ts
@@ -1,4 +1,16 @@
+import { FleetComponent } from './fleet.component';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-@NgModule({})
+const fleetRoutes: Routes = [
+  {
+    path: '',
+    component: FleetComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(fleetRoutes)],
+  exports: [RouterModule],
+})
 export class FleetRoutingModule {}
