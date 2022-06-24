05886d3df29c36a1f4d68519b1f801f11a0f1e6f
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-06-24 09:46:38 +0430

    created fleet component

--- src/app/modules/fleet/fleet.module.ts
+++ src/app/modules/fleet/fleet.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FleetComponent } from './fleet.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FleetComponent
+  ],
   imports: [
     CommonModule
   ]
