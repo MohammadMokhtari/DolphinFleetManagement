c0c234309f65e35833a02abb9e3cd2aff703514a
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-06-25 10:08:05 +0430

    created fleetSidenavService

--- src/app/modules/fleet/fleet.module.ts
+++ src/app/modules/fleet/fleet.module.ts
@@ -7,9 +7,16 @@ import { SharedModule } from '../shared/shared.module';
 import { FleetUnitsTableComponent } from './components/fleet-units-table/fleet-units-table.component';
 import { FleetUnitDetailComponent } from './components/fleet-unit-detail/fleet-unit-detail.component';
 import { FleetUnitDetailMapComponent } from './components/fleet-unit-detail/fleet-unit-detail-map/fleet-unit-detail-map.component';
+import { FleetSidenavService } from './services/fleet-sidenav.service';
 
 @NgModule({
-  declarations: [FleetComponent, FleetUnitsTableComponent, FleetUnitDetailComponent, FleetUnitDetailMapComponent],
+  declarations: [
+    FleetComponent,
+    FleetUnitsTableComponent,
+    FleetUnitDetailComponent,
+    FleetUnitDetailMapComponent,
+  ],
   imports: [CommonModule, FleetRoutingModule, SharedModule],
+  providers: [FleetSidenavService],
 })
 export class FleetModule {}
---
+++ src/app/modules/fleet/services/fleet-sidenav.service.ts
@@ -0,0 +1,22 @@
+import { MatSidenav } from '@angular/material/sidenav';
+
+export class FleetSidenavService {
+  private sidenav: MatSidenav;
+
+  public setSidenav(sidenav: MatSidenav) {
+    this.sidenav = sidenav;
+  }
+
+  public open() {
+    return this.sidenav.open();
+  }
+
+  public close() {
+    return this.sidenav.close();
+  }
+
+  public toggle(): void {
+    this.sidenav.toggle();
+    console.log(this.sidenav);
+  }
+}
