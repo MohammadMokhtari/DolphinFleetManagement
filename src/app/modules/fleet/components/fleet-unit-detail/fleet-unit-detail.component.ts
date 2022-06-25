93252f34dd2bad45940fe5d401063c2e10349a62
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-06-25 10:36:33 +0430

    injected fleetSidenavService in fleetUnitDetial component and call close Fn

--- src/app/modules/fleet/components/fleet-unit-detail/fleet-unit-detail.component.ts
+++ src/app/modules/fleet/components/fleet-unit-detail/fleet-unit-detail.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FleetSidenavService } from '../../services/fleet-sidenav.service';
 
 @Component({
   selector: 'app-fleet-unit-detail',
   templateUrl: './fleet-unit-detail.component.html',
-  styleUrls: ['./fleet-unit-detail.component.scss']
+  styleUrls: ['./fleet-unit-detail.component.scss'],
 })
 export class FleetUnitDetailComponent implements OnInit {
+  constructor(private fleetSidenavService: FleetSidenavService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  closeDrawer() {
+    this.fleetSidenavService.close();
   }
-
 }
