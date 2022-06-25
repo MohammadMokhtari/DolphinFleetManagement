64233ed65df1124ba638a01572a78aea77ca9b67
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-06-25 10:11:41 +0430

    injected fleetSidenavService and set sidenave component in fleet compoenent

--- src/app/modules/fleet/fleet.component.ts
+++ src/app/modules/fleet/fleet.component.ts
@@ -1,12 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { FleetSidenavService } from './services/fleet-sidenav.service';
 
 @Component({
   selector: 'app-fleet',
   templateUrl: './fleet.component.html',
   styleUrls: ['./fleet.component.scss'],
 })
-export class FleetComponent implements OnInit {
-  constructor() {}
+export class FleetComponent implements OnInit, AfterViewInit {
+  constructor(private fleetSidenavService: FleetSidenavService) {}
+
+  @ViewChild('fleetSidenav') public fleetSidenav: MatSidenav;
+
+  ngAfterViewInit(): void {
+    this.fleetSidenavService.setSidenav(this.fleetSidenav);
+  }
 
   ngOnInit(): void {}
 }
