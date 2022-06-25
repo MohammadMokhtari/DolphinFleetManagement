3b86c3b8d5aa7b25b12496a750a6b36d896388e2
Mohammad Mokhthtari <dev.mohammadmokhtari@gmail.com>
2022-06-25 10:30:50 +0430

    config mapbox-gl in fleetUnitDetailMap component

--- src/app/modules/fleet/components/fleet-unit-detail/fleet-unit-detail-map/fleet-unit-detail-map.component.ts
+++ src/app/modules/fleet/components/fleet-unit-detail/fleet-unit-detail-map/fleet-unit-detail-map.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { environment } from 'src/environments/environment';
+import * as Mapboxgl from 'mapbox-gl';
+
 @Component({
   selector: 'app-fleet-unit-detail-map',
   templateUrl: './fleet-unit-detail-map.component.html',
-  styleUrls: ['./fleet-unit-detail-map.component.scss']
+  styleUrls: ['./fleet-unit-detail-map.component.scss'],
 })
 export class FleetUnitDetailMapComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  private unitDetailMap: mapboxgl.Map;
 
   ngOnInit(): void {
+    setTimeout(() => {
+      (Mapboxgl.accessToken as any) = environment.mapbox.accessToken;
+      this.unitDetailMap = new Mapboxgl.Map({
+        container: 'unit-detial-map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [51.4467248, 32.4850885],
+        zoom: 8.5,
+      });
+      this.unitDetailMap.on('load', () => {
+        this.unitDetailMap.resize();
+      });
+    }, 200);
   }
-
 }
